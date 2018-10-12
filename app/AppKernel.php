3793b2e865086546838b7b076c93ff1a5e72d770
Evgeniy-Goroh <evgeniy.goroh@gmail.com>
2018-10-12 14:56:09 +0000

    add FOSUserBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,8 +17,10 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
             new AppBundle\AppBundle(),
             new BookBundle\BookBundle(),
+            
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
